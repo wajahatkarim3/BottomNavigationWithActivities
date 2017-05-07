a92af041689e3af85a3460e79a200f426b4682b9
Wajahat Karim <wajahatkarim3@gmil.com>
2017-05-08 01:57:27 +0500

    Added Line Selector like zomato app....

--- app/src/main/java/com/wajahatkarim3/bottomnavactivities/BottomActivityWithCenter.java
+++ app/src/main/java/com/wajahatkarim3/bottomnavactivities/BottomActivityWithCenter.java
@@ -3,12 +3,27 @@ package com.wajahatkarim3.bottomnavactivities;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import static android.R.attr.shape;
+import static com.wajahatkarim3.bottomnavactivities.R.id.bnve;
+import static com.wajahatkarim3.bottomnavactivities.R.id.text;
 
 public class BottomActivityWithCenter extends AppCompatActivity {
 
@@ -36,7 +51,7 @@ public class BottomActivityWithCenter extends AppCompatActivity {
         bnve.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
+                selectItemAt(item);
                 return true;
             }
         });
@@ -48,6 +63,44 @@ public class BottomActivityWithCenter extends AppCompatActivity {
             }
         });
 
+        addSelectorLineAt(0);
+        addSelectorLineAt(1);
+        addSelectorLineAt(2);
+        addSelectorLineAt(3);
+        addSelectorLineAt(4);
 
+        selectItemAt(bnve.getMenu().getItem(0));
     }
+
+    public void addSelectorLineAt(int position)
+    {
+        //bnve.getBottomNavigationItemView(position).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        View v = new View(this);
+        v.setId(R.id.line_view_id);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                10
+        );
+        params.gravity = Gravity.BOTTOM;
+        v.setLayoutParams(params);
+        v.setBackgroundColor(Color.parseColor("#cb202d"));
+        v.setVisibility(View.INVISIBLE);
+        bnve.getBottomNavigationItemView(position).addView(v);
+    }
+
+    public void selectItemAt(MenuItem item)
+    {
+        int position = bnve.getMenuItemPosition(item);
+        hidePreviousLines();
+        bnve.getBottomNavigationItemView(position).findViewById(R.id.line_view_id).setVisibility(View.VISIBLE);
+    }
+
+    public void hidePreviousLines()
+    {
+        for (int i=0; i<bnve.getItemCount(); i++)
+        {
+            bnve.getBottomNavigationItemView(i).findViewById(R.id.line_view_id).setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
