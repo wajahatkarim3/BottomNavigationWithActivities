254ecaa5fa0e4cacc31be3eee2fb17d7539d2eb3
Wajahat Karim <wajahatkarim3@gmail.com>
2017-05-18 14:38:27 +0500

    Bottom menu done...

--- app/src/main/java/com/wajahatkarim3/bottomnavactivities/ExampleActivity.java
+++ app/src/main/java/com/wajahatkarim3/bottomnavactivities/ExampleActivity.java
@@ -2,6 +2,8 @@ package com.wajahatkarim3.bottomnavactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class ExampleActivity extends PlusMenuActivity {
 
@@ -11,5 +13,12 @@ public class ExampleActivity extends PlusMenuActivity {
         //setContentView(R.layout.activity_example);
         setContentLayout(R.layout.activity_example);
 
+
+
+    }
+
+    public void onReviewClick(View view)
+    {
+        showMenu(false);
     }
 }
